e02050c6bf53e966680ca264a60f6a5874343a2d
Phu-Le <ledoanphu94@gmail.com>
2025-03-01 11:42:12 +0700

    Init Vue-3 Udemy Project

---
+++ event-booking/src/components/BookingItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <SectionCard>
+        <div class="flex justify-between">
+            <div class="flex space-x-1">
+                <div> {{ title }} -</div>
+                <div>status: {{ status }} </div>
+            </div>
+            <RoundButton @click="$emit('cancel')" variant="danger">Cancel</RoundButton>
+        </div>
+        
+    </SectionCard>
+</template>
+
+<script setup>
+import SectionCard from "@/components/SectionCard.vue"
+import RoundButton from "@/components/RoundButton.vue";
+defineProps({
+    title: String,
+    status: String,
+});
+</script>
---
+++ event-booking/src/components/LoadingBookingCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <SectionCard class="animate-pulse">
+      <div class="flex justify-between items-center">
+        <div class="flex space-x-4 w-full">
+            <div class="h-4 w-1/3 bg-gray-300 rounded" ></div>
+            <div class="h-4 w-24 bg-gray-300 rounded" ></div>
+        </div>
+        <div class="h-8 w-18 bg-gray-300 rounded-full"></div>
+      </div>
+    </SectionCard>
+  </template>
+<script setup>
+import SectionCard from '@/components/SectionCard.vue';
+</script>
